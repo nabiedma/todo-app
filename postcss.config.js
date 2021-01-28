37df09f6e994edbdbc1ec05adb3191158326c644
nabiedma <nabiedma@gmail.com>
2021-01-28 17:34:01 -0300

    starting

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        // Some useful comment
+        fontFamily: {
+          'ubuntu': ['ubuntu', 'sans-serif'],
+        },
+    },
+    plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ]};
